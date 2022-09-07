545d330937e3a37868e33e1bdf0e96b42c177d48
Ignacio Rene <ignacio.rene1994@gmail.com>
2022-09-07 00:09:16 -0300

    agregado searchbar y animaciones, junto con mejoras en el main

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// MENU ANIMATIONS
 const hamburger = document.querySelector(".hamburger");
 const navMenu=document.querySelector(".menu");
 
@@ -10,3 +11,23 @@ document.querySelectorAll('.menu-link').forEach(n => n.addEventListener('click',
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }));
+
+//SEARCHBAR ANIMATIONS
+
+const searchInput=document.querySelector('.searchbar-input');
+const searchButton=document.querySelector('.searchbar-box');
+const searchCancel=document.querySelector('.searchbar-cancel-box');
+
+searchButton.addEventListener('click', ()=>{
+  searchInput.classList.toggle('active');
+  searchButton.classList.toggle('active');
+  searchCancel.classList.toggle('active');  
+})
+
+searchCancel.addEventListener('click', ()=>{
+    searchInput.classList.remove('active');
+    searchButton.classList.remove('active');
+    searchCancel.classList.remove('active');
+
+    searchInput.querySelector('input').value='';
+})
